b1af81f4a4dd86d9cbe4ce837b63cb8925e90d45
Nattawat Ujjin <nattawau@ais.co.th>
2019-03-06 15:09:08 +0700

    change from /metrics to /

--- 01-helloworld/service1.js
+++ 01-helloworld/service1.js
@@ -4,6 +4,6 @@ const app = express()
 const port = 9999
 
 
-app.get('/metrics', (req, res) => res.send(("Hello World")) )
+app.get('/', (req, res) => res.send(("Hello World")) )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
