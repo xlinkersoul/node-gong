aa2f325b461fd6dab61185bee2d3ed0828ac88ca
Nattawat Ujjin <nattawau@ais.co.th>
2019-03-07 16:01:11 +0700

    Add Reame.md

--- 03-helloworld_function_metrics/README.md
+++ 03-helloworld_function_metrics/README.md
@@ -1,8 +1,12 @@
 ## Goal
 
-Create Function in Node.JS
+Write first Metrics using Node.JS
 
 ## Steps
 
 1. `npm install` and `npm start`
 2. Call http://127.0.0.1:9999
+3. Start Prometheus
+4. Query by `equinox_node_result`
+5. Query `equinox_node_result {code="2001"}`
+6. Query `equinox_node_result {server="OCF3g402"}`
--- 03-helloworld_function_metrics/service1.js
+++ 03-helloworld_function_metrics/service1.js
@@ -4,6 +4,14 @@ const app = express()
 const port = 9999
 
 function my_function(){
+	console.log('entering my_function')
+	var message="Hello World"
+	console.log('finish my_function')
+	console.log('---------------------------------')
+	return message
+}
+
+function gen_stat(){
 
 	var message="# Equinox Custom Stat\n"
 	message = message + 'equinox_node_result{service="OCF", server="OCF3g402",code="2001"} 999\n'
@@ -12,5 +20,6 @@ function my_function(){
 }
 
 app.get('/', (req, res) => res.send(( my_function() )) )
+app.get('/metrics', (req, res) => res.send(( gen_stat() )) )
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
