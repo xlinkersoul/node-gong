4cb29cea8feaabd8d0e67e97de3fa17908b61a84
Nattawat Ujjin <nattawau@ais.co.th>
2019-03-07 19:41:29 +0700

    Add 05

---
+++ 05-sine-wave-by-minute/README.md
@@ -0,0 +1,9 @@
+## Goal
+
+get Minute using Node.JS
+
+## Steps
+
+1. `npm install` and `npm start`
+2. Call http://127.0.0.1:9999/sine
+
---
+++ 05-sine-wave-by-minute/service1.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const util = require('util')
+const app = express()
+const port = 9999
+
+function my_function(){
+	console.log('entering my_function')
+	var message="Hello World"
+	console.log('finish my_function')
+	console.log('---------------------------------')
+	return message
+}
+function gen_stat(){
+
+	var message="# Equinox Custom Stat\n"
+	message = message + 'equinox_node_result{service="OCF", server="OCF3g402",code="2001"} 999\n'
+	message = message + 'equinox_node_result{service="OCF", server="OCF3g402",code="5012"} 2\n'
+	return message
+}
+function sinWaveByMinute(){
+	var message="#Sine Web\n"
+	
+	// Get Minute
+	var dateClass=new Date();
+	var now_minute=dateClass.getMinutes();
+	console.log('Console Now Min : '+now_minute);
+	message = message + "Web Now Minute : "+now_minute;
+		
+	return message;
+}
+
+app.get('/', (req, res) => res.send(( my_function() )) )
+app.get('/metrics', (req, res) => res.send(( gen_stat() )) )
+app.get('/sine', (req, res) => res.send(( sinWaveByMinute() )) )
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
