32ca31afbe96943752eceb8609b862ed6f992e49
Nattawat Ujjin <nattawau@ais.co.th>
2019-03-07 20:25:52 +0700

    OK

--- 05-sine-wave-by-minute/README.md
+++ 05-sine-wave-by-minute/README.md
@@ -6,4 +6,8 @@ get Minute using Node.JS
 
 1. `npm install` and `npm start`
 2. Call http://127.0.0.1:9999/sine
+3. Call http://127.0.0.1:9999/metrics
+4. Create Metrics in Prometheus http://127.0.0.1:9090
+4. Create Graph in Grafana http://127.0.0.1:3000
 
+![alt text](https://github.com/xlinkersoul/node-gong/tree/master/05-sine-wave-by-minute/sine_wave.png)
--- 05-sine-wave-by-minute/service1.js
+++ 05-sine-wave-by-minute/service1.js
@@ -15,6 +15,7 @@ function gen_stat(){
 	var message="# Equinox Custom Stat\n"
 	message = message + 'equinox_node_result{service="OCF", server="OCF3g402",code="2001"} 999\n'
 	message = message + 'equinox_node_result{service="OCF", server="OCF3g402",code="5012"} 2\n'
+	
 	return message
 }
 function sinWaveByMinute(){
@@ -25,12 +26,33 @@ function sinWaveByMinute(){
 	var now_minute=dateClass.getMinutes();
 	console.log('Console Now Min : '+now_minute);
 	message = message + "Web Now Minute : "+now_minute;
-		
+	
+	// convert Minute to degree
+	// 60 x 6 = 360, so to convert Minute to Degree 
+	//, we need to multiply minute by 6
+	var degree=now_minute*6;
+	console.log('Console Now Degree : '+degree);
+	message = message + "Web Now Degree : "+degree;
+	
+	// convert degree to radian
+	// 1 degree = 0.0174533 radian
+	var radian=degree*0.0174533;
+	console.log('Console Now radian : '+radian);
+	message = message + "Web Now radian : "+radian;	
+	
+	// get Sin Value
+	var sinValue = Math.sin(radian);
+	console.log('Console Now sinValue : '+sinValue);
+	message = message + "Web Now sinValue : "+sinValue;		
+	
+	message = 'my_sin_wave_1{service="sine"} '+sinValue+'\n'
+	
 	return message;
 }
 
 app.get('/', (req, res) => res.send(( my_function() )) )
-app.get('/metrics', (req, res) => res.send(( gen_stat() )) )
+app.get('/metrics', (req, res) => res.send(( sinWaveByMinute() )) )
 app.get('/sine', (req, res) => res.send(( sinWaveByMinute() )) )
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
